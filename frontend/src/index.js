a4af6bdcb548eef156402b2e155a64098c2c4f24
Soumeya <ks_mekki@esi.dz>
2024-04-07 11:04:43 +0100

    [refactor] chnge router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,34 @@
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import React, { Component } from "react";
 import {
   BrowserRouter as Router,
-  HashRouter,
-  Route,
   Routes,
-  Switch,
-} from "react-router-dom";
-import Home from "./components/pages/home";
+  Route,
+} from 'react-router-dom';
+import Home from './components/pages/home';
 import ForumPage from './components/pages/ForumPage';
 import ThreadPage from './components/pages/ThreadPage';
 
-class Root extends Component {
-  render() {
-    return (
-      <div id="App">
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path='forum' element = {<ForumPage />}/>
-            <Route path='forum/threads/:thread_id' element={<ThreadPage/>}/>
-          </Routes>
-        </Router>
-      </div>
-    );
-  }
+function Root() {
+  return (
+    <div id="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/forum" element={<ForumPage />} />
+          <Route path="/forum/threads/:thread_id" element={<ThreadPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
-export default Root;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
+  <React.StrictMode>
     <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
