c90f5e88d0f8ac1190edda9b241c4214bd8bc976
Soumeya <ks_mekki@esi.dz>
2024-04-16 21:33:11 +0100

    [refactor] db name

--- MAPI/MAPI/settings.py
+++ MAPI/MAPI/settings.py
@@ -97,14 +97,11 @@ DATABASES = {
     'default': {
 
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'search',
+        'NAME': 'mapi',
         'USER': 'root',
         'PASSWORD': '',
         'HOST':'localhost',
-        'PORT':'3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'PORT':'3306'
     }
 }
 
