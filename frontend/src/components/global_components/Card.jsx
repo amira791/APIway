f28555aff0d365f2181e39711ddf368ede78bade
Soumeya <ks_mekki@esi.dz>
2024-04-17 10:07:46 +0100

    [refactor] functinal tests

--- frontend/src/components/global_components/Card.jsx
+++ frontend/src/components/global_components/Card.jsx
@@ -30,28 +30,30 @@ const Card = ({ apiName, description, logo, termsOfUse, website, categoryLabel, 
                     </div>
                 </div>
                 <div className="features">
-                    <div className="product-media" style={{ height: '200px' }}>
-                        <img src={logo} alt="API Logo" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                    <div className="product-media">
+                        <img src="assets/images/apiLogo.jpg" alt="API Logo" />
                     </div>
                 </div>
                 <div className="bottom">
                     <div className="details-product">
                         <div className="author">
                             <div className="content">
+                              {/*  <div className="position">Terms Of Use</div>
+                                <div className="name"> <a href="#">{termsOfUse}</a></div>*/}
                                 <div className="position">Category</div>
                                 <div className="name"> <a href="#">{categoryLabel}</a></div>
+                                
                             </div>
                         </div>
                         <div className="current-bid">
                             <div className="subtitle">Price</div>
                             <div className="price">
-                                <span className="cash">5000 DZA</span><span className="icon"><img src="assets/images/icon/ethe.svg" alt="images" /></span>
+                                <span className="cash">5000 DZA</span><span className="icon"><img src="assets/images/icon/ethe.svg" alt="images"/></span>
                             </div>
                         </div>
                     </div>
                     <div className="product-button">
-                        {/* Use Link instead of a regular anchor tag */}
-                        <NavLink to={`/ApiDetail/${idApi}`} className="tf-button flex items-center">
+                        <a href="#" data-toggle="modal" data-target="#popup_bid" className="tf-button flex items-center">
                             <span className="fas fa-info-circle" style={{ marginRight: '-15px' }}></span>
                            See Details
                         </a>
