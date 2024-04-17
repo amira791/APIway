e5151183d28d0b22c5dd1c6a278807ddf6b3b893
Soumeya <ks_mekki@esi.dz>
2024-04-17 12:07:56 +0100

    testCoverge75

--- MAPI/MAPI/settings.py
+++ MAPI/MAPI/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
     'default': {
 
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mapi',
+        'NAME': 'search',
         'USER': 'root',
         'PASSWORD': '',
         'HOST':'localhost',
